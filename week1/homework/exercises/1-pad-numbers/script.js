660ec9397adfcdc2940f5f4a2b5307bdc853bf16
atabek-bakh <coderonline@icloud.com>
2021-01-31 03:33:43 +0100

    finished exercise 1, 2

--- week1/homework/exercises/1-pad-numbers/script.js
+++ week1/homework/exercises/1-pad-numbers/script.js
@@ -16,6 +16,15 @@
  *   where to use `exports` and where `require`?
  */
 
-let numbers = [ "12", "846", "2", "1236" ];
+let numbers = ["12", "846", "2", "1236"];
 
 // YOUR CODE GOES HERE
+const padLeft = require('./padLeft.js');
+
+const paddedNumbers = numbers.forEach(element => {
+  console.log(padLeft(element, 5, '_'));
+});
+
+console.log(paddedNumbers);
+
+
--- week1/homework/exercises/2-left-pad/script.js
+++ week1/homework/exercises/2-left-pad/script.js
@@ -8,6 +8,13 @@
  *
  */
 
-let numbers = [ "12", "846", "2", "1236" ];
+let numbers = ["12", "846", "2", "1236"];
 
 // YOUR CODE GOES HERE
+const leftPad = require('left-pad');
+
+const leftPaddedElements = numbers.map(element => {
+  return leftPad(element, 5, '_');
+});
+
+console.log(leftPaddedElements);
