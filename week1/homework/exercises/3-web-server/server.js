28d7c36bc30a9358b09740b8d41343589e9ac1ba
atabek-bakh <coderonline@icloud.com>
2021-02-03 20:24:25 +0100

    added error handler function

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -6,39 +6,47 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
+function handleError(res, mistake) {
+  if (mistake.code === 'ENOENT') {
+    fs.readFile(path.join(__dirname, 'public', '404.html'), (fileNotFoundError, content) => {
+      if (fileNotFoundError) {
+        res.writeHead(500);
+        res.end(`404 error file not found: ${fileNotFoundError.message}`);
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf8');
+      }
+    });
+  } else {
+    res.writeHead(500);
+    res.end(`Server error ${mistake.code}`);
+  }
+}
+
 const server = http.createServer((req, res) => {
-	let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
-	let extName = path.extname(filePath);
+  const filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const extName = path.extname(filePath);
 
-	fs.readFile(filePath, (err, content) => {
+  fs.readFile(filePath, (err, content) => {
+    const extensions = {
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.json': 'application/json',
+      '.png': 'image/png',
+      '.jpg': 'image/jpg',
+    };
 
-		const extensions = {
-			'.js': 'text/javascript',
-			'.css': 'text/css',
-			'.json': 'application/json',
-			'.png': 'image/png',
-			'.jpg': 'image/jpg'
-		};
-
-		if (err) {
-			if (err.code === 'ENOENT') {
-				fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-					res.writeHead(200, { 'Content-Type': 'text/html' });
-					res.end(content, 'utf8');
-				})
-			} else {
-				res.writeHead(500);
-				res.end(`Server error ${err.code}`);
-			}
-		} else {
-			res.writeHead(200, {
-				'Content-Type': extensions[extName] || 'text/html'
-			});
-			res.end(content, 'utf8');
-		}
-	});
+    if (err) {
+      handleError(res, err);
+    } else {
+      res.writeHead(200, {
+        'Content-Type': extensions[extName] || 'text/html',
+      });
+      res.end(content, 'utf8');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`server is running on ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
