a8a8b12b2b978e55589b787a06cb1586a9e46b16
atabek-bakh <coderonline@icloud.com>
2021-02-06 22:10:38 +0100

    finished the back-end of the project

--- week2/homework/hackyourtemperature/server.js
+++ week2/homework/hackyourtemperature/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const handlebars  = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.set('view engine', 'handlebars');
 
@@ -12,9 +16,14 @@ app.engine('handlebars', handlebars({
 app.use(express.static('css'));
 
 app.get('/', (req, res) => {
-  res.render('main', {layout : 'main'});
+  res.render('index', {layout : 'index'});
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) });
+
+app.post('/weather', function (req, res) { 
+  res.send('You typed ' + req.body.cityName);
+  res.status(201);
+});
