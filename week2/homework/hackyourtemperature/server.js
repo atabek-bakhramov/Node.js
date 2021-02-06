541f370128389e6e2e1b6c5147ba35bfff5ac1e5
atabek-bakh <coderonline@icloud.com>
2021-02-06 22:29:46 +0100

    added some changes to the structure of the server.js file

--- week2/homework/hackyourtemperature/server.js
+++ week2/homework/hackyourtemperature/server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
-
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.set('view engine', 'handlebars');
@@ -19,11 +18,13 @@ app.get('/', (req, res) => {
   res.render('index', {layout : 'index'});
 });
 
+app.post('/weather', function (req, res) { 
+  const { cityName } = req.body;
+  res.type('application/json');
+  res.send(JSON.stringify(cityName));
+  res.status(201);
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) });
-
-app.post('/weather', function (req, res) { 
-  res.send('You typed ' + req.body.cityName);
-  res.status(201);
-});
