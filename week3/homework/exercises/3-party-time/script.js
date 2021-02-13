bc30ed56fd9701334774497531bad383ed59b955
atabek-bakh <coderonline@icloud.com>
2021-02-13 18:38:21 +0100

    finished exercise 3

--- week3/homework/exercises/3-party-time/script.js
+++ week3/homework/exercises/3-party-time/script.js
@@ -9,8 +9,33 @@
  * - make sure to use the correct headers and http method in the request
  */
 
-function makeReservation() {
-  // YOUR CODE GOES IN HERE
-}
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+const makeReservation = () => {
+  app.post('/', async (req, res) => {
+    try {
+      const reservationData = {
+        name: 'Atabek',
+        numberOfPeople: 5
+      };
+      const reservationDataStringified = JSON.stringify(reservationData);
+      const response = await fetch('https://reservation100-sandbox.mxapps.io/api/reservations',
+        {
+          method: 'POST',
+          body:    reservationDataStringified,
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
+};
 
 makeReservation();
+
+app.listen(3000);
