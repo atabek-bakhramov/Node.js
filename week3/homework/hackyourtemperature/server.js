e0fa0504f5260c2b41856697f706791bc697ef00
atabek-bakh <coderonline@icloud.com>
2021-02-15 20:59:02 +0100

    added styling and replaced axios with fetch-node

--- week3/homework/hackyourtemperature/server.js
+++ week3/homework/hackyourtemperature/server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const axios = require('axios');
-const bodyParser = require('body-parser');
+import express from 'express';
+import exphbs from 'express-handlebars';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+dotenv.config();
 
 const app = express();
 
@@ -11,28 +13,26 @@ app.use(express.static('public'));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
- 
-const API_KEY = require('./sources/keys.json').API_KEY;
 
 app.get('/', (req, res) => {
     res.render('index', { layout: 'main' });
 });
 
-app.post('/weather', (req, res) => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityName}&appid=${API_KEY}&units=metric`)
-        .then(response => {
-            res.status(200);
-            res.render('index', {
-                temperature: `In ${req.body.cityName} currently ${response.data.main.temp}°`,
-                description: `${response.data.weather[0].main}`,
-                windSpeed: `Wind Speed is ${response.data.wind.speed} m/s`
-            })
+app.post('/weather', async (req, res) => {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityName}&appid=${process.env.API_KEY}&units=metric`);
+        const dataJson = await response.json();
+        res.status(200);
+        res.render('index', {
+            temperature: `In ${req.body.cityName} ${dataJson.main.temp} °C`,
+            description: `${dataJson.weather[0].main}`,
+            windSpeed: `Wind Speed: ${dataJson.wind.speed} m/s`
         })
-        .catch(error => {
-            res.status(404);
-            res.render('404', { layout: 'error' });
-        })
-});
+    } catch(error) {
+        res.status(404);
+        res.render('404', { layout: 'error' });
+    }
+})
 
 app.get('*', (req, res) => {
     res.render('404', { layout: 'error' });
